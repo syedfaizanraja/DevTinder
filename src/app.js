ef95e0a5aa633b478fa9215ea9224e9da3f19718
syedfaizanraja <syedfaizanraja@gmail.com>
2024-09-28 14:15:12 +0530

    Exploring get,post API

--- src/app.js
+++ src/app.js
@@ -3,9 +3,26 @@ const express = require("express");
 
 const app = express(); // start the server
 
+//This will only handles GET calls to /user
+app.get("/user", (req,res) =>{
+    res.send("User Data - Faizan");
+});
+
+app.post("/user", (req,res) => {
+    //Logic to store the data in DB.
+    res.send("Data stored successfully.");
+});
+
+// This will match the all the HTTP methods API calls to /test
 app.use("/test", (req,res) => {
     res.send("Hello From Test");
 });
+
+// Sequence is very important.
+app.use("/", (req,res) => {
+    res.send("Hello om Test");
+});
+
 
 app.listen(7777, () => {
     console.log("Server is started");
